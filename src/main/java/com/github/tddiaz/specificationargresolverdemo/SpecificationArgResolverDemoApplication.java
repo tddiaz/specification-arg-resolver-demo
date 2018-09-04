00853d497e6a7c8cf24c8d647729d5f7f0e368d1
tddiaz <tristantomdiaz@gmail.com>
2018-09-04 21:32:11 +0400

    initial commit

---
+++ src/main/java/com/github/tddiaz/specificationargresolverdemo/SpecificationArgResolverDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.tddiaz.specificationargresolverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationArgResolverDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificationArgResolverDemoApplication.class, args);
+	}
+}
