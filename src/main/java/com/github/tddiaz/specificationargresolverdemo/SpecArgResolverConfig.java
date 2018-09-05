ed5b2b917e8b07d3512b3cc20fd936f5ee45fac9
tddiaz <tristantomdiaz@gmail.com>
2018-09-05 10:27:08 +0400

    added custom localdate spec

--- src/main/java/com/github/tddiaz/specificationargresolverdemo/SpecArgResolverConfig.java
+++
@@ -1,7 +0,0 @@
-package com.github.tddiaz.specificationargresolverdemo;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-public class SpecArgResolverConfig implements WebMvcConfigurer {
-}
